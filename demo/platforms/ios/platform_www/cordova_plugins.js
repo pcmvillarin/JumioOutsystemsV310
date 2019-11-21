0476b8334464a86d28ef1f8e7c5254548f198453
Paulv <pcm_villarin@yahoo.com>
2019-11-21 15:34:04 +0800

    Update Plugin Configuration
    Update Plugin Configuration

---
+++ demo/platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-jumio-mobilesdk.JumioMobileSDK",
+      "file": "plugins/cordova-plugin-jumio-mobilesdk/www/JumioMobileSDK.js",
+      "pluginId": "cordova-plugin-jumio-mobilesdk",
+      "clobbers": [
+        "Jumio"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-jumio-mobilesdk": "3.1.0",
+    "cordova-plugin-whitelist": "1.3.3"
+  };
+});
